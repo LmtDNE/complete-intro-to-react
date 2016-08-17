ff5c857c101a4649f1cc084b8936c80480e24830
Dion <Dion.Fulwood@gmail.com>
2016-08-17 15:56:27 -0700

    Initializes set up of webpack

--- js/ClientApp.js
+++ js/ClientApp.js
@@ -1,27 +1,17 @@
+var React = require('react')
+var ReactDOM = require('react-dom')
+var MyTitle = require('./myTitle')
 var div = React.DOM.div
-var h1 = React.DOM.h1
-
-var MyTitle = React.createClass({
-  render () {
-    return (
-      div(null, 
-          h1(null, this.props.title)
-        )
-    )
-  }
-})
 
 var MyTitleFactory = React.createFactory(MyTitle)
 var cE = React.createElement
 
-
 var MyFirstComponenet = (
-  div({style:{color:'red'}},
-      MyTitleFactory({title: 'Props are great'}),
-      cE(MyTitle, {title: 'Use props everywhere!'}),
-      cE(MyTitle, {title: 'Props are best!'})
-
+  div({style: {color: 'red'}},
+    MyTitleFactory({title: 'Props are great', color: 'rebeccapurple'}),
+    cE(MyTitle, {title: 'Use props everywhere!', color: 'peru'}),
+    cE(MyTitle, {title: 'Props are best!', color: 'mediumaquamarine'})
     )
-)
+  )
 
 ReactDOM.render(MyFirstComponenet, document.getElementById('app'))
